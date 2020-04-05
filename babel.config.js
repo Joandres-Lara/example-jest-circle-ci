a9fb181b1504d041db3e17d2d02104beeb0ebe12
Joan Andrés Lara Mora <joandres52752lm@gmail.com>
2020-04-04 21:33:35 -0600

    Configuración para manejar la sintaxis `async` `await`

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,14 @@
-const __DEV__ = process.env.NODE_ENV !== 'production';
-const __TEST__ = process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'testing';
 /**
  * Configuración para el present de @babel/preset-env.
  * 
  * @var {object} configPresetBabelEnv
  */
 const configPresetBabelEnv = {
- loose: true,
- useBuiltIns: 'usage'
+ useBuiltsIn: 'entry',
+ exclude: ['@babel/plugin-proposal-dynamic-import'],
+ shippedProposals: true,
+ targets: {node: 'current'},
 };
-
-if(__TEST__){
- /**
-  * Configuración para los test de Jest.
-  * Para más información ver: 
-  * https://jestjs.io/docs/en/getting-started#using-babel
-  */
- configPresetBabelEnv.targets = {
-  node: 'current'
- }
-}
 /**
  * Configuración para babel
  * 
@@ -27,27 +16,17 @@ if(__TEST__){
  */
 module.exports = function configureBabel(api){
  api.cache(false);
-
  return {
   presets: [
    require('@babel/preset-env', configPresetBabelEnv),
    require('@babel/preset-react'),
-   require('babel-preset-fbjs')
+   require('babel-preset-fbjs'),
   ],
   plugins: [
-   require('babel-plugin-styled-components', {
-    displayName: __DEV__,
-    minify: __DEV__,
-    transpileTemplateLiterals: __DEV__,
-    ssr: false
-   }),
-   require('@babel/plugin-transform-async-to-generator'),
-   require('@babel/plugin-proposal-class-properties', { loose: true }),
-   require('@babel/plugin-syntax-dynamic-import'),
    require('@babel/plugin-transform-runtime', {
-    corejs: 3
+    corejs: 2
    }),
-   require('@babel/plugin-transform-react-jsx-source')
+   require('@babel/plugin-syntax-dynamic-import'),
   ]
  }
 }
--- src/__tests__/testeable-fn.test.js
+++ src/__tests__/testeable-fn.test.js
@@ -1,12 +1,17 @@
 import testeableFn from '../testeable-fn';
+// Por ahora jest, no incluye automáticamente estos
+// módulos, así que hay que incluirlos manualmente.
+import Promise from "@babel/runtime-corejs3/core-js-stable/promise";
 
 describe('initial test', () => {
  it('test first', () => {
   expect(testeableFn()).toBeTruthy();
  })
-
- it('test async', async () => {
-  const moduleAsync = await import('../testeable-fn-async');
-  expect(moduleAsync()).toBeTruthy();
- })
 });
+
+
+test('test async', async () => {
+ let moduleAsync = await import('../testeable-fn-async');
+ moduleAsync = moduleAsync.default;
+ expect(moduleAsync()).toBeTruthy();
+})
